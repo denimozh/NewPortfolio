fa16ab707f86f888df171385f50f32dead7a1992
denimozh <denimozh@gmail.com>
2024-02-03 23:05:18 +0000

    Finished the About Me Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,26 @@
 "use client"
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Hero from "../components/Hero"
 import Projects from "../components/Projects"
 import Header from "../components/Header"
 import Preloader from "../components/Preloader"
+import About from "../components/About"
 import Cursor from "@/components/Cursor/Cursor";
 import { AnimatePresence } from "framer-motion";
+
 
 export default function Home() {
 
   const [isLoading, setIsloading] = useState(true);
+  const paragraph1 = "Hi I'm Denis : A FrontEnd Developer - Based in London, UK. My TechStack Includes : NextJS, Typescript, React, TailwindCSS. I also know how to expertly use Docker and Github."
+
+  const path = useRef(null);
+  let progress = 0;
+  let time = Math.PI / 2;
+  let reqId = null;
+  let x = 0.5;
   useEffect( () => {
     (
       async () => {
@@ -20,10 +29,55 @@ export default function Home() {
 
         setTimeout( () => {
           setIsloading(false);
-        }, 2000)
+        }, 1400)
       }
     )()
+    setPath(progress);
   }, [])  
+
+  const setPath = (progress) => {
+    const { innerWidth } = window;
+    const width = innerWidth;
+    path.current.setAttributeNS("", "d", `M0 50 Q${width * x} ${50 + progress}, ${width} 50`)
+  }
+
+  const manageMouseEnter = () => {
+    if(reqId){
+      window.cancelAnimationFrame(reqId);
+      resetAnimation();
+    }
+  }
+
+  const manageMouseMove = (e) => {
+    const { movementY, clientX } = e;
+    const { left, width } = path.current.getBoundingClientRect();
+    x = (clientX - left) / width;
+    progress += movementY;
+    setPath(progress)
+  }
+
+  const manageMouseLeave = () => {
+    animateOut();
+  }
+
+  const lerp = (x, y, a) => x * (1 - a) + y * a
+
+  const animateOut = () => {
+    const newProgress = progress * Math.sin(time);
+    time += 0.2;
+    setPath(newProgress);
+    progress = lerp(progress, 0, 0.01);
+    if(Math.abs(progress) > 0.75){
+      reqId = window.requestAnimationFrame(animateOut);
+    } else {
+      resetAnimation();
+    }
+  }
+
+  const resetAnimation = () => {
+    time = Math.PI / 2;
+    progress = 0;
+  }
 
   return (
     <main className={styles.main}>
@@ -35,6 +89,15 @@ export default function Home() {
       <Header/>
       <Cursor/>
       <Hero/>
+      <div style={{height:"30vh"}}></div>
+      <About value={paragraph1}/>
+      <div className={styles.line}>
+        <div onMouseEnter={manageMouseEnter} onMouseMove={manageMouseMove} onMouseLeave={manageMouseLeave} className={styles.box}></div>
+          <svg>
+            <path ref={path}></path>
+          </svg>
+      </div>
+      <div style={{height:"30vh"}}></div>
       <Projects />
     </main>
   );
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div>index
-      <p>1234</p>
-      <p>1234</p>
-      <p>1234</p>
-      <p>1234</p>
-      <p>1234</p>
+    <div>
+      projects
     </div>
   )
 }
